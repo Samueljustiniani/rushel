fa7ea0c54aaf041de930db7478257d1fa350e516
Samuel Imanol <167726711+Samueljustiniani@users.noreply.github.com>
2024-11-26 00:49:56 -0500

    asdasdads

--- database.js
+++ database.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2');
 
 // Configuración de conexión con RDS MySQL
 const connection = mysql.createConnection({
-    host: 'ods1.c9svexml9zrk.us-east-1.rds.amazonaws.com', // Cambia por tu host RDS
+    host: 'finalapobreza.c920g66kq4od.us-east-1.rds.amazonaws.com', // Cambia por tu host RDS
     user: 'admin',               // Cambia por tu usuario de RDS
     password: 'samuel123',        // Cambia por tu contraseña
-    database: 'ods'       // Cambia por tu base de datos
+    database: 'comentariosDB'       // Cambia por tu base de datos
 });
 
 connection.connect((err) => {
